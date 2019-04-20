8eff7bdd274659e7821f92cb3688613d36159351
Gustavo Caetano <gustavo@storychief.io>
2019-04-20 18:35:08 -0300

    add fields

---
+++ src/models/Settings.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace storychief\storychiefv3\models;
+
+use craft\base\Model;
+
+class Settings extends Model
+{
+    public $key;
+    public $section;
+    public $entry_type;
+    public $mapping = null;
+    public $custom_field_definitions = [];
+
+    public function rules()
+    {
+        return [
+          [['key', 'section', 'entry_type'], 'string'],
+          [['key', 'section', 'entry_type'], 'required'],
+          [['custom_field_definitions', 'mapping'], 'mixed'],
+    ];
+    }
+}
