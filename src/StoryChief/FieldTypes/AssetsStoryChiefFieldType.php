27e07ade9f887d28ab57026071310c01b9fb3f7d
Gustavo Caetano <gustavo@storychief.io>
2019-05-01 01:06:26 -0300

    fix asseets

--- src/StoryChief/FieldTypes/AssetsStoryChiefFieldType.php
+++ src/StoryChief/FieldTypes/AssetsStoryChiefFieldType.php
@@ -1,6 +1,9 @@
 <?php namespace storychief\storychiefv3\storychief\FieldTypes;
 
+use Craft;
 use  craft\base\Field;
+use craft\elements\Asset;
+use craft\helpers\Assets;
 
 class AssetsStoryChiefFieldType implements StoryChiefFieldTypeInterface
 {
@@ -14,25 +17,35 @@ class AssetsStoryChiefFieldType implements StoryChiefFieldTypeInterface
 
     public function prepFieldData(Field $field, $fieldData)
     {
-        $folderId = $field->getFieldType()->resolveSourcePath();
+        if ($field->useSingleFolder) {
+            $volumeUID = explode(':', $field->singleUploadLocationSource)[1];
+        } else {
+            $volumeUID = explode(':', $field->defaultUploadLocationSource)[1];
+        }
+        $volumeID = Craft::$app->getVolumes()->getVolumeByUid($volumeUID)->id;
+        
         $preppedData = [];
+        
+        $tempFolder = Craft::$app->getPath()->tempAssetUploadsPath;
 
         // get remote image and store in temp path
         $imageInfo = pathinfo($fieldData);
-        $tempPath = CRAFT_STORAGE_PATH . 'runtime/temp/' . $imageInfo['basename'];
+        $tempPath = $tempFolder . $imageInfo['basename'];
         file_put_contents($tempPath, fopen($fieldData, 'r'));
 
-        // insert the file into assets
-        $response = craft()->assets->insertFileByLocalPath(
-            $tempPath,
-            $imageInfo['basename'],
-            $folderId, // notice, this is the id of the folder you want to upload to
-            AssetConflictResolution::KeepBoth
-        );
+        $filename = Assets::prepareAssetName($imageInfo['basename']);
+
+        $asset = new Asset();
+        $asset->tempFilePath = $tempPath;
+        $asset->filename = $filename;
+        $asset->folderId = $volumeID;
+        $asset->avoidFilenameConflicts = true;
+        $response = Craft::$app->elements->saveElement($asset);
+
 
         // if the response is a success, get the file id
-        if ($response && $response->isSuccess()) {
-            $preppedData[] = $response->getDataItem('fileId');
+        if ($response) {
+            $preppedData[] = $asset->id;
         }
 
         return $preppedData;
