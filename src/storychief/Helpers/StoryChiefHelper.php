0e5069cd668e7f13c99d61f14a7aa1ede27a2543
gregory <gregory@storychief.io>
2021-12-27 14:54:33 +0100

    Refactor StoryChiefHelper@getStoryChiefFieldClass() for better readability

--- src/storychief/Helpers/StoryChiefHelper.php
+++ src/storychief/Helpers/StoryChiefHelper.php
@@ -1,6 +1,26 @@
 <?php namespace storychief\storychiefv3\storychief\Helpers;
 
+use craft\fields\Assets;
+use craft\fields\Categories;
+use craft\fields\Checkboxes;
+use craft\fields\Dropdown;
+use craft\fields\Entries;
+use craft\fields\Lightswitch;
+use craft\fields\MultiSelect;
+use craft\fields\PlainText;
+use craft\fields\RadioButtons;
+use craft\fields\Tags;
+use storychief\storychiefv3\storychief\FieldTypes\AssetsStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\CategoriesStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\CheckboxesStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\DropdownStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\EntriesStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\LightswitchStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\MultiSelectStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\PlainTextStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\RadioButtonsStoryChiefFieldType;
 use storychief\storychiefv3\storychief\FieldTypes\RichTextStoryChiefFieldType;
+use storychief\storychiefv3\storychief\FieldTypes\TagsStoryChiefFieldType;
 
 class StoryChiefHelper
 {
@@ -47,10 +67,34 @@ class StoryChiefHelper
             return null;
         }
 
-        if (class_exists(\craft\redactor\Field::class) && $field instanceof \craft\redactor\Field) {
-            return RichTextStoryChiefFieldType::class;
-        }
+        switch (get_class($field)) {
+            // Default available fields
+            case Assets::class:
+                return AssetsStoryChiefFieldType::class;
+            case Categories::class:
+                return CategoriesStoryChiefFieldType::class;
+            case Tags::class:
+                return TagsStoryChiefFieldType::class;
+            case Entries::class:
+                return EntriesStoryChiefFieldType::class;
+            case Checkboxes::class:
+                return CheckboxesStoryChiefFieldType::class;
+            case RadioButtons::class:
+                return RadioButtonsStoryChiefFieldType::class;
+            case Dropdown::class:
+                return DropdownStoryChiefFieldType::class;
+            case Lightswitch::class:
+                return LightswitchStoryChiefFieldType::class;
+            case MultiSelect::class:
+                return MultiSelectStoryChiefFieldType::class;
+            case PlainText::class:
+                return PlainTextStoryChiefFieldType::class;
 
-        return str_replace('craft\\fields', '\\storychief\\storychiefv3\\storychief\\FieldTypes', get_class($field)) . 'StoryChiefFieldType';
+            // Plugin installed fields
+            case 'craft\redactor\Field':
+                return RichTextStoryChiefFieldType::class;
+            default:
+                return null;
+        }
     }
 }
